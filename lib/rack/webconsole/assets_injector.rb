fc2721d7509c142fbbbcaf67f6f9115a1dd2ca12
Rishab Govind <rishab.govind@ilabsolutions.com>
2014-11-12 18:31:33 -0500

    added an on-demand asset injector

---
+++ lib/rack/webconsole/assets_injector.rb
@@ -0,0 +1,54 @@
+# encoding: utf-8
+module Rack
+  class Webconsole
+    # {AssetsInjector} is a Rack middleware responsible for injecting the webconsole code asynchronously
+    #
+    class AssetsInjector
+      include Webconsole::AssetHelpers
+
+      # Honor the Rack contract by saving the passed Rack application in an ivar.
+      #
+      # @param [Rack::Application] app the previous Rack application in the
+      #   middleware chain.
+      def initialize(app)
+        @app = app
+      end
+
+      
+      # @param [Hash] env a Rack request environment.
+      def call(env)
+
+        #status, headers, response = (@app.call(env) rescue [200, {}, ""])
+
+        #req = Rack::Request.new(env)
+        #params = req.params
+
+        #return [status, headers, response] unless check_legitimate(req)
+        script = render(asset('webconsole.js'),
+                 :TOKEN => Webconsole::Repl.reset_token,
+                 :KEY_CODE => Webconsole.key_code,
+                 :CONTEXT => env['SCRIPT_NAME'] || "")
+
+        response_body = MultiJson.encode({:value => "#{code(env)}", :js => script})
+        headers = {}
+        headers['Content-Type'] = 'application/json'
+        headers['Content-Length'] = response_body.bytesize.to_s
+        [200, headers, [response_body]]
+      end
+
+      # Returns a string with all the HTML, CSS and JavaScript code needed for
+      # the view.
+      #
+      # It puts the security token inside the JavaScript to make AJAX calls
+      # secure.
+      #
+      # @return [String] the injectable code.
+      def code(env)
+        html_code <<
+          css_code
+          
+      end
+
+    end
+  end
+end
